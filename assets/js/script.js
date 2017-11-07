e09ee2dd4031fba978e48e298d6b7d9a4911c1da
vabramov <valeriy.abramov@faceit.com.ua>
2017-11-07 16:34:25 +0200

    fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,5 @@
 $(document).ready(function () {
 
-  $('.main-wrapper').on('load scroll', function () {
-    var scrolled = $(this).scrollTop();
-    $('#headerVideo').css('transform', 'translate3d(0, ' + (scrolled * 0.25) + 'px, 0)'); // parallax (25% scroll rate)
-  });
-
-  /*$("body").niceScroll({
-    scrollspeed: 50,
-  });*/
-
-  $('.main-wrapper').on('load scroll', function () {
-      console.log(scrolledUp);
-      var scrolledUp = $(this).scrollTop();
-      var headerVideo = $('.header-video ').height();
-      if (scrolledUp >= headerVideo) {
-          $("#videoBottom").addClass('video-bottom_fixed');
-      } else {
-          $("#videoBottom").removeClass('video-bottom_fixed');
-      }
-  });
-
     function checkWidth() {
         var windowSize = $(window).width();
 
@@ -46,10 +26,6 @@ $(document).ready(function () {
                 $('#headerVideo').css('transform', 'translate3d(0, ' + (scrolled * 0.25) + 'px, 0)'); // parallax (25% scroll rate)
             });
 
-            /*$("body").niceScroll({
-             scrollspeed: 50,
-             });*/
-
             $(window).on('load scroll', function () {
                 console.log(scrolledUp);
                 var scrolledUp = $(this).scrollTop();
@@ -68,5 +44,5 @@ $(document).ready(function () {
     // Bind event listener
     $(window).resize(checkWidth);
 
-  $("video.header-video").get(0).play();
+    $("video.header-video").get(0).play();
 });
